f8c552f2462b74ddca820f84c745ada3e6695afd
Alex Chapman <3729326+ajxchapman@users.noreply.github.com>
2021-02-07 11:14:42 +0000

    Delete local.sh

--- local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-JEKYLL_VERSION=3.8
-docker run -it --rm \
-  -p 4000:4000 \
-  --volume="$PWD:/srv/jekyll" \
-  --volume="$PWD/.bundle:/usr/local/bundle" \
-  jekyll/jekyll:$JEKYLL_VERSION \
-  jekyll serve --drafts $*
